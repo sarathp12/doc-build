7cf07effe3b6087b555552e8eef7886ed1b8495e
root <root@localhost.localdomain>
2018-09-06 13:56:31 -0400

    initial shell script added

---
+++ firstexec.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cp /home/jenkins/*.war /home/jenkins/workspace/doc-build
+
+cd /var/lib/jenkins/workspace/doc-build
+
+docker build -t webserver:$(build_number) .
+
+
+docker run -it --rm -p 9095:8080 --name webproject webserver:$(build_number) 
