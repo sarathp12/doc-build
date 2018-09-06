7c7a3ef6931b7481cdf481e2b1a04a8028dadcbd
root <root@localhost.localdomain>
2018-09-06 14:12:45 -0400

    sudo added and environment bvaraible corrected

--- firstexec.sh
+++ firstexec.sh
@@ -3,7 +3,7 @@
 cp /home/jenkins/*.war /home/jenkins/workspace/doc-build
 
 
-docker build -t webserver:v$(BUILD_NUMBER) .
+sudo docker build -t webserver:v$BUILD_NUMBER .
 
 
-docker run -it --rm -p 9095:8080 --name webproject webserver:v$(BUILD_NUMBER) 
+sudo docker run -it --rm -p 9095:8080 --name webproject webserver:v$(BUILD_NUMBER) 
