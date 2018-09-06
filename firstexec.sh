3668226af79c84a33e0860c54dc03ffb8b16c10c
root <root@localhost.localdomain>
2018-09-06 14:15:25 -0400

    environment variable modified

--- firstexec.sh
+++ firstexec.sh
@@ -3,7 +3,7 @@
 cp /home/jenkins/*.war /home/jenkins/workspace/doc-build
 
 
-sudo docker build -t webserver:v$BUILD_NUMBER .
+sudo docker build -t webserver:v$(env.BUILD_NUMBER) .
 
 
-sudo docker run -it --rm -p 9095:8080 --name webproject webserver:v$(BUILD_NUMBER) 
+sudo docker run -it --rm -p 9095:8080 --name webproject webserver:v$(env.BUILD_NUMBER) 
