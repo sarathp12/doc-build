8a24d5ca3f68f678e8121312b151f69568e40efc
root <root@localhost.localdomain>
2018-09-06 14:05:46 -0400

    Environment variable coreected with upper case

--- firstexec.sh
+++ firstexec.sh
@@ -3,7 +3,7 @@
 cp /home/jenkins/*.war /home/jenkins/workspace/doc-build
 
 
-docker build -t webserver:$(build_number) .
+docker build -t webserver:v$(BUILD_NUMBER) .
 
 
-docker run -it --rm -p 9095:8080 --name webproject webserver:$(build_number) 
+docker run -it --rm -p 9095:8080 --name webproject webserver:v$(BUILD_NUMBER) 
